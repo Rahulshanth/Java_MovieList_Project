3d44a051e513902789700cbaa57978d6b7459646
Rahulshanth <yogarajahrahulshanth@gmail.com>
2026-03-01 23:28:58 +0530

    init comit

--- src/pk1/Movie.java
+++ src/pk1/Movie.java
@@ -2,6 +2,16 @@ package pk1;
 
 public class Movie implements Comparable <Movie>{
 
+    private String movieId;
+    private String title;
+    private String director;
+    private int releaseYear;
+    private double rating;
+    private String language;
+    private Category category;
+
+    private static int ID = 001;
+
     @Override
     public int compareTo(Movie other) {
         // Sort by rating in DESCENDING order (5 stars first, 1 star last)
