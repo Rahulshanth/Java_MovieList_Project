92278fc415826c04c36bcf5152613c5c0e7a2582
Rahulshanth <yogarajahrahulshanth@gmail.com>
2026-03-08 20:58:20 +0530

    init comit

--- src/pk1/Movie.java
+++ src/pk1/Movie.java
@@ -18,4 +18,73 @@ public class Movie implements Comparable <Movie>{
         return 0;
                 //Double.compare(other.rating, this.rating);
     }
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+
+    // SETTERS - Modify private fields from outside (WRITE)
+    // No setter for movieId because it should never change after creation
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "\n" +
+                        " ID       : %-28s \n" + movieId,
+                        " Title    : %-28s \n" + title,
+                        "Director : %-28s \n" + director,
+                        "Year     : %-28d \n" + releaseYear,
+                        "Rating   : %-28.1f/5.0 \n" + rating,
+                        "Language : %-28s \n" + language,
+                        "Category : %-28s \n" + category
+        );
+    }
 }
